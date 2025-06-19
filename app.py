53775f78f9a9f836931d55bef3619387d9d0856f
Damien Mascheix <damien.mascheix@michelin.com>
2025-06-19 09:45:20 +0000

    Add initial implementation of Wine Quality Prediction App

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+# this is the main function in which we define our webpage  
+def main():
+    st.markdown("# Wine Quality Prediction App 🍷🍇")
+    st.markdown("### This app is meant to predict red wine quality " +
+            "according to different chemical")
+
+# Init code
+if __name__=='__main__': 
+    main()
