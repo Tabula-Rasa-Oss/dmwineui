8d6c2968e2db90d4dfb0dbb62893a86a64f05056
Damien Mascheix <damien.mascheix@michelin.com>
2025-06-19 12:02:29 +0000

    Rename package from 'yourwineui' to 'dmwineui' in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
     content = f.readlines()
 requirements = [x.strip() for x in content if 'git+' not in x]
 
-setup(name='yourwineui',
+setup(name='dmwineui',
       version="1.0",
       description="Project Red Wine Quality Prediction Web App",
       packages=find_packages(),
